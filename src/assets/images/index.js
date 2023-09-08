129de9706af5f4e26b0be99514546750f11806d2
Qmanh <quanmanh567@gmail.com>
2023-09-08 22:29:45 +0700

    header 2

--- src/assets/images/index.js
+++ src/assets/images/index.js
@@ -2,4 +2,8 @@ const images = {
   logo: require('~/assets/images/logo.svg').default,
 };
 
-export default images;
+const images1 = {
+  logo: require('~/assets/images/chipi.jpg'),
+};
+
+export { images, images1 };
